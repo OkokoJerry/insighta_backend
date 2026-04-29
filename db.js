a9b10cae4d036d554cfa180bd62a5aff771aa643
Okoko Jerry <okokojerry159@gmail.com>
2026-04-29 19:27:48 +0100

    added env config

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { Pool } = require('pg');
-
+require('dotenv').config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: process.env.DB_SSL === 'false' ? false : { rejectUnauthorized: false },
